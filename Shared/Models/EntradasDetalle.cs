ba4571f9c64a2303f815b875477426ca56f82395
Kissland Baker <Kissland_baker@ucne.edu.do>
2023-07-10 19:57:55 -0400

    Creando el detalle

--- Shared/Models/EntradasDetalle.cs
+++ Shared/Models/EntradasDetalle.cs
@@ -2,5 +2,8 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 public class EntradasDetalle
 {
-    
+    public int DetalleId {get; set;}
+    public int EntradaId {get; set;}
+    public int ProductoId {get; set;}
+    public string CantidadUtilizada {get; set;}
 }
