004a14bf13b971012b9dec6140fa8937424f53ce
Kissland Baker <Kissland_baker@ucne.edu.do>
2023-07-10 20:43:03 -0400

    Terminando el Context

--- Server/DAL/Context.cs
+++ Server/DAL/Context.cs
@@ -1,1 +1,67 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options) { }
+    public DbSet<Entradas> entradas { get; set; }
+
+    public DbSet<Productos> productos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Productos>().HasData
+        (
+            new Productos
+            {
+                ProductoId = 1,
+                Descripcion ="Mani",
+                Tipo = 0,
+                Existencia = 50,
+            },
+            new Productos
+            {
+                ProductoId = 2,
+                Descripcion ="Pistachos",
+                Tipo = 0,
+                Existencia = 600,
+            },
+            new Productos
+            {
+                ProductoId = 3,
+                Descripcion ="Pasas",
+                Tipo = 0,
+                Existencia = 500,
+            },
+            new Productos
+            {
+                ProductoId = 4,
+                Descripcion ="Ciruelas",
+                Tipo = 0,
+                Existencia = 700,
+            },
+            new Productos
+            {
+                ProductoId = 5,
+                Descripcion ="Mixto MPP 0.5 lb",
+                Tipo = 1,
+                Existencia = 0,
+            },
+            new Productos
+            {
+                ProductoId = 6,
+                Descripcion ="Mixto MPC 0.5 lb",
+                Tipo = 1,
+                Existencia = 0,
+            },
+            new Productos
+            {
+                ProductoId = 7,
+                Descripcion ="Mixto MPP 0.2 lb",
+                Tipo = 1,
+                Existencia = 0,
+            }
+        );
+    }
+}
