ea74499414a79bf1ec7bf728329093f34c0f8baf
Kissland Baker <Kissland_baker@ucne.edu.do>
2023-07-11 00:41:37 -0400

    Eliminando lineas de codigo innecesarias

--- Server/DAL/Context.cs
+++ Server/DAL/Context.cs
@@ -14,54 +14,19 @@ public class Context : DbContext
         modelBuilder.Entity<Productos>().HasData(new List<Productos>()
         {
             new Productos
-            {
-                ProductoId = 1,
-                Descripcion ="Mani",
-                Tipo = 0,
-                Existencia = 50,
-            },
+            {ProductoId = 1, Descripcion ="Mani", Tipo = 0, Existencia = 50,},
             new Productos
-            {
-                ProductoId = 2,
-                Descripcion ="Pistachos",
-                Tipo = 0,
-                Existencia = 600,
-            },
+            {ProductoId = 2, Descripcion ="Pistachos", Tipo = 0, Existencia = 600,},
             new Productos
-            {
-                ProductoId = 3,
-                Descripcion ="Pasas",
-                Tipo = 0,
-                Existencia = 500,
-            },
+            {ProductoId = 3, Descripcion ="Pasas", Tipo = 0, Existencia = 500,},
             new Productos
-            {
-                ProductoId = 4,
-                Descripcion ="Ciruelas",
-                Tipo = 0,
-                Existencia = 700,
-            },
+            {ProductoId = 4, Descripcion ="Ciruelas", Tipo = 0, Existencia = 700,},
             new Productos
-            {
-                ProductoId = 5,
-                Descripcion ="Mixto MPP 0.5 lb",
-                Tipo = 1,
-                Existencia = 0,
-            },
+            {ProductoId = 5, Descripcion ="Mixto MPP 0.5 lb", Tipo = 1, Existencia = 0,},
             new Productos
-            {
-                ProductoId = 6,
-                Descripcion ="Mixto MPC 0.5 lb",
-                Tipo = 1,
-                Existencia = 0,
-            },
+            {ProductoId = 6, Descripcion ="Mixto MPC 0.5 lb", Tipo = 1, Existencia = 0,},
             new Productos
-            {
-                ProductoId = 7,
-                Descripcion ="Mixto MPP 0.2 lb",
-                Tipo = 1,
-                Existencia = 0,
-            }
+            {ProductoId = 7, Descripcion ="Mixto MPP 0.2 lb", Tipo = 1, Existencia = 0,}
         });
     }
 }
